9dd60e7d7d93882d4b93e7f029e01bc857e81bad
iver56 <ij@aiascience.com>
2018-08-03 09:48:25 +0200

    Generate images with text instead of sine waves

--- generate_images.py
+++ generate_images.py
@@ -1,21 +1,36 @@
-import math
 import os
+import math
 
-import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
+
+shape = (224, 224)
+
+fonts = {}
+
+
+def get_font(font_size):
+    if font_size not in fonts:
+        fonts[font_size] = ImageFont.truetype("BebasNeue-Regular.ttf", font_size)
+    return fonts[font_size]
+
 
 # Generate and store images
 for i in tqdm(range(50)):
-    wavelength = 4 + 0.1 * i * i
-    frequency = 1 / wavelength
-    shape = (224, 224)
-    img = np.zeros(shape, dtype=np.float32)
-    for y in range(shape[0]):
-        for x in range(shape[1]):
-            img[y][x] = 0.5 + 0.5 * math.sin(math.pi * 2 * frequency * x)
 
-    img = img * 255
-    img_int = np.uint8(img)
-    pil_image = Image.fromarray(img_int)
-    pil_image.save(os.path.join("data", "{0:04d}.png".format(i)))
+    image = Image.new("RGB", shape, (255, 255, 255))
+
+    # Drawing context
+    ctx = ImageDraw.Draw(image)
+
+    font_size = math.ceil(image.size[0] / 4)
+    font = get_font(font_size)
+
+    ctx.text(
+        (font_size, int(image.size[1] - 1.666 * font_size)),
+        str(i),
+        font=font,
+        fill=(20, 20, 20, 20),
+    )
+
+    image.save(os.path.join("data", "{0:04d}.png".format(i)))
